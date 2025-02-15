bb91156a2800fc63f088c00d0b06be3577e7ab95
Mark Shivers <shivers.mark@gmail.com>
2025-02-15 11:15:22 -0500

    Adding spring initializr files

---
+++ src/main/java/com/markshivers/branchtakehome/BranchtakehomeApplication.java
@@ -0,0 +1,13 @@
+package com.markshivers.branchtakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchtakehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchtakehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markshivers/branchtakehome/BranchtakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markshivers.branchtakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchtakehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
